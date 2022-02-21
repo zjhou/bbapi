57c8340a12e1f542b3f8f45ecaeee3423c374a62
bbbottle <z@zjh.im>
2022-02-21 09:19:29 +0800

    fix(projects): filter projects by status

--- api/projects.ts
+++ api/projects.ts
@@ -3,7 +3,10 @@ import { supabase } from "./_supabase";
 import allowCors from "./_cors";
 
 async function handler(request: VercelRequest, response: VercelResponse) {
-  const { data, error } = await supabase.from("project").select("name, id");
+  const { data, error } = await supabase
+    .from("project")
+    .select("name, id, status")
+    .eq("status", 1);
   response.status(error ? 500 : 200).json(data);
 }
 
