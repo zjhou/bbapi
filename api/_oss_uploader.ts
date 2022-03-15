c24a334a88a7055b147cdc73ec1d12e760378183
bbbottle <z@zjh.im>
2022-03-16 01:56:09 +0800

    refactor: use env

--- api/_oss_uploader.ts
+++ api/_oss_uploader.ts
@@ -1,7 +1,7 @@
 import OSS from "ali-oss";
 
 const client = new OSS({
-  region: "oss-cn-shenzhen",
+  region: process.env.OSS_REGION,
   accessKeyId: process.env.OSS_ACCESS_KEY_ID as string,
   accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET as string,
   bucket: process.env.OSS_BUCKET_NAME,
