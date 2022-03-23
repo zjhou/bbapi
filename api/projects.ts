76e7f5b06002e7db6c2bca8ac49ca2af36b6e84c
bbbottle <z@zjh.im>
2022-03-23 14:22:59 +0800

    fix: order

--- api/projects.ts
+++ api/projects.ts
@@ -5,7 +5,8 @@ import allowCors from "./_cors";
 async function handler(request: VercelRequest, response: VercelResponse) {
   const { data, error } = await supabase
     .from("project")
-    .select("name, id, status, description")
+    .select("name, id, status, description, order")
+    .order("order")
     .eq("status", 1);
   response.status(error ? 500 : 200).json(data);
 }
