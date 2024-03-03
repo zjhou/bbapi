010bb9163df1a1cb941a1fab2801dfa5d31b0665
bbbottle <z@zjh.im>
2024-03-04 02:25:55 +0800

    fix: params

--- api/post.ts
+++ api/post.ts
@@ -3,18 +3,16 @@ import { supabase } from "./_supabase";
 import allowCors from "./_cors";
 
 async function handler(request: VercelRequest, response: VercelResponse) {
-  const { title, content } = request.body;
+  const { title, content } = JSON.parse(request.body);
 
   const titleWithoutExtension = title.split(".")[0];
 
   if (!title || !content) {
-    console.log(title + " | " + content);
     response.status(400).json({ error: "Title and content are required" });
     return;
   }
 
   const { data, error } = await supabase.from("post").insert({ title: titleWithoutExtension, content });
-  console.log(error);
   response.status(error ? 500 : 200).json(data);
 }
 
