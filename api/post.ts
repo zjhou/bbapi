20a6ebf0a1ab778f24c0b908ec877194a6661b4a
bbbottle <z@zjh.im>
2024-03-04 03:36:36 +0800

    feat: md 2 html

--- api/post.ts
+++ api/post.ts
@@ -1,6 +1,9 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { supabase } from "./_supabase";
 import allowCors from "./_cors";
+import showdown from "showdown";
+
+const converter = new showdown.Converter();
 
 async function handler(request: VercelRequest, response: VercelResponse) {
   const { title, content } = JSON.parse(request.body);
@@ -12,7 +15,12 @@ async function handler(request: VercelRequest, response: VercelResponse) {
     return;
   }
 
-  const { data, error } = await supabase.from("post").insert({ title: titleWithoutExtension, content });
+  const { data, error } = await supabase.from("post")
+    .insert({
+      title: titleWithoutExtension,
+      content: converter.makeHtml(content)
+    });
+
   response.status(error ? 500 : 200).json(data);
 }
 
