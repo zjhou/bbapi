f3621b502f486789f468fd20bd443b1326779ae1
bbbottle <z@zjh.im>
2024-03-04 02:07:04 +0800

    fix: params

--- api/post.ts
+++ api/post.ts
@@ -3,11 +3,13 @@ import { supabase } from "./_supabase";
 import allowCors from "./_cors";
 
 async function handler(request: VercelRequest, response: VercelResponse) {
-  const { title, content } = request.query;
-  if (!title || !content) {
+  const { arg } = request.body;
+  if (!arg || !arg.title || !arg.content) {
     response.status(400).json({ error: "Title and content are required" });
     return;
   }
+
+  const { title, content } = arg;
 
   const { data, error } = await supabase.from("post").insert({ title, content });
   console.log(error);
