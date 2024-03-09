9150d2af89de934b6734a9806dd125739c822a66
bbbottle <z@zjh.im>
2024-03-09 17:02:13 +0800

    feat: add remove api

---
+++ api/remove_post.ts
@@ -0,0 +1,26 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { supabase } from "./_supabase";
+import allowCors from "./_cors";
+
+async function handler(request: VercelRequest, response: VercelResponse) {
+  const { title } = JSON.parse(request.body);
+
+  if (!title) {
+    response.status(400).json({ error: "title missing" });
+    return;
+  }
+
+  const { data, error } = await supabase.from("post")
+    .delete()
+    .eq("title", title);
+
+  if (error) {
+    console.log(error);
+    response.status(500).json({ error: "Something went wrong" });
+    return
+  }
+
+  response.status(200).json(data[0]);
+}
+
+export default allowCors(handler);
